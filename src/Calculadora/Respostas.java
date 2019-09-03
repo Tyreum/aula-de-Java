5eb60d232fd9982e78b89ca7e34c516bc026b138
LABORATORIO_INFO <LABORATORIO_INFO@10.67.96.54>
2019-09-03 19:55:41 -0300

    Calc finalizado

--- src/Calculadora/Respostas.java
+++ src/Calculadora/Respostas.java
@@ -1,7 +1,16 @@
 package Calculadora;
 
 import javax.swing.*;
-
+import Calculadora.Entrada;
+import Calculadora.Operacoes;
+        
+        
+        
 public class Respostas {
+    
+    public static void resp(){
+        
+        JOptionPane.showMessageDialog(null, Entrada.resultado);
+}
     
 }
