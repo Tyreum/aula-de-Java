d80ef36cfe84ce17b88976a26e9381d2e5a97e64
Tyreum <>
2019-09-03 22:19:34 -0300

    Calc finalizado de fato

--- src/Calculadora/Entrada.java
+++ src/Calculadora/Entrada.java
@@ -6,31 +6,69 @@ import static Calculadora.Operacoes.soma;
 import static Calculadora.Operacoes.subtracao;
 import static Calculadora.Operacoes.multiplicacao;
 import static Calculadora.Operacoes.divisao;
+import static Calculadora.Respostas.resp;
 
 public class Entrada {
 
     public static void main(String[] args) {
-        double num1 = 0, num2 = 0;
+        double num1 = 0, num2 = 0, resultado = 0;
         int escolha = 0;
-        String aux = JOptionPane.showInputDialog(null, "Selecione uma operação\n\n 1-Soma\n 2-Subtração\n 3-Multiplicação\n 4-Divisão\n\n");
+        String opera = "", sinal = "";
+        String aux = JOptionPane.showInputDialog(null, "Selecione uma operação\n\n 1-Soma\n 2-Subtração\n 3-Multiplicação\n 4-Divisão\n 5-Sair\n\n ");
         escolha = Integer.parseInt(aux);
 
         switch (escolha) {
-            case 1: aux = JOptionPane.showInputDialog(null,"Digite um número");
-                    num1 = Double.parseDouble(aux);
-                    aux = JOptionPane.showInputDialog(null,"Digite um número");
-                    num2 = Double.parseDouble(aux);
-                    soma(num1, num2);
-                    JOptionPane.showMessageDialog(null, resultado);
+            case 1:
+                aux = JOptionPane.showInputDialog(null, "Digite um número");
+                aux = aux.replace(",", ".");
+                num1 = Double.parseDouble(aux);
+                aux = JOptionPane.showInputDialog(null, "Digite um número");
+                aux = aux.replace(",", ".");
+                num2 = Double.parseDouble(aux);
+                resultado = Operacoes.soma(num1, num2);
+                opera = "Soma";
+                sinal = "+";
                 break;
-            case 2:;
+            case 2:
+                aux = JOptionPane.showInputDialog(null, "Digite um número");
+                aux = aux.replace(",", ".");
+                num1 = Double.parseDouble(aux);
+                aux = JOptionPane.showInputDialog(null, "Digite um número");
+                aux = aux.replace(",", ".");
+                num2 = Double.parseDouble(aux);
+                resultado = Operacoes.subtracao(num1, num2);
+                opera = "Subtração";
+                sinal = "-";
                 break;
-            case 3:;
+            case 3:
+                aux = JOptionPane.showInputDialog(null, "Digite um número");
+                aux = aux.replace(",", ".");
+                num1 = Double.parseDouble(aux);
+                aux = JOptionPane.showInputDialog(null, "Digite um número");
+                aux = aux.replace(",", ".");
+                num2 = Double.parseDouble(aux);
+                resultado = Operacoes.multiplicacao(num1, num2);
+                opera = "Multiplicação";
+                sinal = "*";
                 break;
-            case 4:;
+            case 4:
+                aux = JOptionPane.showInputDialog(null, "Digite um número");
+                aux = aux.replace(",", ".");
+                num1 = Double.parseDouble(aux);
+                aux = JOptionPane.showInputDialog(null, "Digite um número");
+                aux = aux.replace(",", ".");
+                num2 = Double.parseDouble(aux);
+                resultado = Operacoes.divisao(num1, num2);
+                opera = "Divisão";
+                sinal = "/";
                 break;
-
+            case 5:
+                System.exit(0);
+            default:;
+            JOptionPane.showMessageDialog(null, "Insira um valor válido!");
+            System.exit(0);
+            break;
         }
+        JOptionPane.showMessageDialog(null, "Valores digitados: \n" + num1 + ", " + num2 + "\n\nOperador Selecionado: \n" + opera + "\n\n A " + opera + " de " + num1 + sinal + num2 + " = " + resultado);
     }
-
 }
